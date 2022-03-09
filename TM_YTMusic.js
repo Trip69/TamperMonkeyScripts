a7cada51aff49395b5b8382e0d0a080574d3e893
Trip69 <chrisbitschi@hotmaill.com>
2022-03-09 09:41:49 +0200

    Added optional reloaded pn click. Using this the window can be minimised and play continues although the song restarts

--- TM_YTMusic.js
+++ TM_YTMusic.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Youtube Music continue watching clicker
 // @namespace   http://tampermonkey.net/
-// @version     0.2
+// @version     0.4
 // @description Clicks continue watching when prompted by you tube music.
 // @author      Trip
 // @icon        https://www.somedodgywebsite.com/favicon.png
@@ -16,35 +16,38 @@
     //console.log("TM_YTMusic: main");
 
     function check_click_button(){
-        //check_click_button.button=document.getElementById("button");
+        if(check_click_button.reload && check_click_button.yes_clicked) {
+            check_click_button.yes_clicked=false;
+            document.location.reload();
+        }
         check_click_button.els=document.getElementsByClassName("style-scope yt-button-renderer style-blue-text");
         if( check_click_button.els.length>1
             && check_click_button.els.item(1).textContent==="Yes") {
             check_click_button.els.item(1).parentElement.click();
+            check_click_button.yes_clicked=true;
         }
         check_click_button.count++;
         //check page host every 10 seconds & stop timer if on another host. Unneeded but tidy.
         if(check_click_button.count===10) {
             on_page = document.location.hostname === "music.youtube.com";
             //console.log("TM_YTMusic: 10 second check");
-            if (!on_page) {
+            if (!on_page)
                 clearInterval(check_click_button.interval);
-            }
             check_click_button.count=0;
         }
     }
     check_click_button.count=0;
     check_click_button.interval=null;
     check_click_button.els=null;
+    check_click_button.reload=true;
+    check_click_button.yes_clicked=false;
 
     function skip_dislike(){
-        if(skip_dislike.button_next_el===null) {
+        if(skip_dislike.button_next_el===null)
             skip_dislike.button_next_el=document.getElementById("left-controls").getElementsByClassName("next-button").item(0);
-        }
         skip_dislike.el=document.getElementById("like-button-renderer");
-        if(skip_dislike.el.firstElementChild.getAttribute("aria-pressed")==="true") {
+        if(skip_dislike.el.firstElementChild.getAttribute("aria-pressed")==="true")
             skip_dislike.button_next_el.click();
-        }
     }
     skip_dislike.interval=null;
     skip_dislike.enabled=true;
