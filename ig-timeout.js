3a789708e5dac49dd384162bf2908664b126d3ae
Trip69 <chrisbitschi@hotmaill.com>
2022-04-21 08:00:21 +0300

    Refresh window every 10 minutes

---
+++ ig-timeout.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         IG Timeout
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Account Timeout Workaround
+// @author       Trip
+// @match        https://*.ig.com/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function openWindow(url) {
+        document.open(url);
+    }
+
+    function reloadWindow() {
+        location.reload();
+    }
+
+    //let url=document.location;
+    //let interval = setInterval(openWindow,10*60*1000,url);
+    let interval = setInterval(reloadWindow,10*60*1000);
+})();
